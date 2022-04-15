cfc52ef1b92e156b54ebf963ed81259f251ecbbc
May Tuse <tusek.may@gmail.com>
2022-04-14 21:44:02 -0700

    Did multiples.

---
+++ MultiplesOfFive/multiples.py
@@ -0,0 +1,10 @@
+"""If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000."""
+
+sumOfValues = 0
+for num in range(1000):
+    if (num % 3 == 0 or num % 5 == 0):
+        sumOfValues += num
+
+print(sumOfValues)
